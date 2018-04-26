68ac78b1ad9837fe5ac31c859e0c4c8c995f85ac
Raynal R. Gobel <raynal.gobel@gmail.com>
2018-04-26 13:24:09 +0700

    STEP - 1.2: Add more components

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,6 +4,17 @@
       <h2>This is Dashboard component</h2>
 
       <!-- list of company will goes here -->
+      <most-active-company />
     </div>
   </div>
 </template>
+
+<script>
+import MostActiveCompany from './dashboard/MostActiveCompany'
+
+export default {
+  components: {
+    MostActiveCompany
+  }
+}
+</script>
---
+++ src/components/dashboard/MostActiveCompany.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="most-active-company card">
+    <div class="card-header">
+      <p>Most Active Company</p>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"
+            v-for="company in mostActiveCompany"
+            :key="company.symbol">
+          <p class="lead">{{ company.symbol }}</p>
+          <p><small>{{ company.companyName }}</small></p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      mostActiveCompany: [
+        { symbol: 'TIME', companyName: 'Time Inc.' },
+        { symbol: 'MARA', companyName: 'Marathon Patent Group Inc.' },
+        { symbol: 'DPW', companyName: 'DPW Holdings Inc.' },
+        { symbol: 'SQ', companyName: 'Square Inc. Class A' }
+      ]
+    }
+  }
+}
+</script>
